0cec4ad78aaff3110e44646b450979c260e8bca8
quinnwai <quinnwai.wong@gmail.com>
2025-11-19 04:50:24 -0800

    patch add-url to work again

--- client/drs-map.go
+++ client/drs-map.go
@@ -56,21 +56,19 @@ func UpdateDrsObjects(logger *Logger) error {
 	}
 	logger.Logf("Preparing %d LFS files out of %d staged files", len(lfsStagedFiles), len(stagedFiles))
 
-	processedDrsIds := map[string]bool{}
 	// Create a DRS object for each staged LFS file
 	// which will be used at push-time
 	for _, file := range lfsStagedFiles {
 		drsId := ComputeDeterministicUUID(file.Name, file.Oid)
-		if processedDrsIds[drsId] {
-			logger.Logf("Skipping staged file %s with OID %s, DRS ID %s already processed in this batch.", file.Name, file.Oid, drsId)
-			continue
-		}
-		processedDrsIds[drsId] = true
 
 		// check if indexd object already prepared, skip if so
 		drsObjPath, err := GetObjectPath(config.DRS_OBJS_PATH, file.Oid, file.Name)
 		if err != nil {
 			return fmt.Errorf("error getting object path for oid %s: %v", file.Oid, err)
+		}
+		if _, err := os.Stat(drsObjPath); err == nil {
+			logger.Logf("Skipping staged file %s with OID %s, DRS object already created at %s", file.Name, file.Oid, drsObjPath)
+			continue
 		}
 
 		// confirm file contents are localized

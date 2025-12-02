ec772237b7018042dc8f5feaff299bd42ec1096a
Brian Walsh <brian@bwalsh.com>
2025-12-01 22:50:56 -0800

    print error msg to stderr

--- git-drs.go
+++ git-drs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/calypr/git-drs/cmd"
@@ -8,6 +9,7 @@ import (
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, "git-drs error:", err)
 		os.Exit(1)
 	}
 }

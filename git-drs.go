212cc5ab1b0fc4e1311cedb4fa1f9d5f0c33ccb3
Brian Walsh <brian@bwalsh.com>
2025-12-02 12:23:49 -0800

    TODO log any errors

--- git-drs.go
+++ git-drs.go
@@ -9,6 +9,7 @@ import (
 
 func main() {
 	if err := cmd.RootCmd.Execute(); err != nil {
+		// is this error getting lost when run in a pre-commit or custom transfer hook?
 		fmt.Fprintln(os.Stderr, "git-drs error:", err)
 		os.Exit(1)
 	}

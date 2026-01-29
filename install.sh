c381ba287d53d15b2305a42542e597e85ccd6763
Liam Beckman <lbeckman314@gmail.com>
2026-01-28 18:58:55 -0800

    debug: Install script tests

--- install.sh
+++ install.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
-# Utility script to download git-drs from GitHub releases
+#
+# Utility script to download git-drs from GitHub releases:
 # https://github.com/calypr/git-drs/releases/latest
 
 REPO="calypr/git-drs"
+echo "Installing $REPO"
 
-echo "Installing git-drs"
-
-# Function to get the latest release URL if no version is provided
+# Latest Release URL
 get_latest_release_url() {
     echo "https://api.github.com/repos/$REPO/releases/latest"
 }
 
-# Function to get the release URL for a specific tag
+# Tag URL 
 get_tag_release_url() {
     echo "https://api.github.com/repos/$REPO/releases/tags/$1"
 }
 
-# Parse version tag argument
+# Optional tag
 VERSION_TAG=$1
 
-# Determine the release URL based on whether a version tag was provided
+# Get release URL
 RELEASE_URL=""
 if [ -z "$VERSION_TAG" ]; then
     RELEASE_URL=$(get_latest_release_url)
     VERSION_TAG=$(curl -s $RELEASE_URL | grep '"tag_name":' | cut -d '"' -f4)
-    echo "No version specified. Fetching the latest release → $VERSION_TAG"
+	if [ -z "$VERSION_TAG" ]; then
+        echo "Failed to fetch the latest release version."
+        exit 1
+    fi
+
+    echo "No version specified, fetching the latest release ($VERSION_TAG)"
 else
     RELEASE_URL=$(get_tag_release_url $VERSION_TAG)
-    echo "Fetching release for version $VERSION_TAG"
 fi
 
-# Normalize version tag by removing 'v' prefix if present
-# This ensures consistency for file naming
+# Remove 'v' prefix
 VERSION_NUMBER="${VERSION_TAG#v}"
 
 # Determine OS and Architecture
@@ -50,25 +53,21 @@ fi
 # Define the tar file based on OS and Architecture
 TAR_FILE="git-drs-${OS}-${ARCH}*.tar.gz"
 CHECKSUM_FILE="git-drs_${VERSION_NUMBER}_checksums.txt"
-
-# Fetch the release assets URLs
 ASSETS=$(curl -s $RELEASE_URL | grep "browser_download_url" | cut -d '"' -f 4)
 
-# Download the tar.gz file and checksums.txt for the detected OS and Arch
-echo "Downloading git-drs for $OS $ARCH"
+# Download executable + checksums
+echo "Downloading git-drs $VERSION_TAG for $OS $ARCH"
 for asset in $ASSETS; do
     if [[ $asset == *"${OS}-${ARCH}"* && $asset == *".tar.gz"* ]]; then
         TAR_URL=$asset
         TAR_NAME=$(basename $asset)
         curl -LsO $TAR_URL
     elif [[ $asset == *"$CHECKSUM_FILE"* ]]; then
-        curl -o $CHECKSUM_FILE -LsO $asset
+        curl -LsO $asset
     fi
 done
 
-# Verify checksum
 echo "Verifying checksum"
-
 CHECKSUM_EXPECTED=$(grep $TAR_NAME $CHECKSUM_FILE | awk '{print $1}')
 
 # Linux
@@ -87,25 +86,21 @@ if [ "$CHECKSUM_EXPECTED" != "$CHECKSUM_ACTUAL" ]; then
     exit 1
 fi
 
-# Extract and install the package
-echo "Extracting the package"
+# Extract + Install
 tar -xzf $TAR_NAME
 
-# Parse installation destination
 DEST=$2
-
-# Determine where to install the git-drs binary
 if [ -z "$DEST" ]; then
     DEST=$HOME/.local/bin
 fi
-echo "Installing git-drs to $DEST"
+echo "Installing to $DEST"
 mkdir -p $DEST
 mv git-drs $DEST
 
-# Check if git-drs is in the user's PATH
+# Check PATH
 if ! command -v git-drs >/dev/null 2>&1; then
 
-	# Check if $DEST is already in PATH
+	# If DEST in not in PATH, then add to PATH
 	if [[ ":$PATH:" != *":$DEST:"* ]]; then
 		echo "Adding $DEST to PATH"
 
@@ -113,7 +108,7 @@ if ! command -v git-drs >/dev/null 2>&1; then
 		if [ -n "$ZSH_VERSION" ]; then
 			SHELL="$HOME/.zshrc"
 		
-		# Default to Bash
+		# Bash
 		else
 			SHELL="$HOME/.bashrc"
 		fi
@@ -123,8 +118,8 @@ if ! command -v git-drs >/dev/null 2>&1; then
 	fi
 fi
 
+echo "Installation successful!"
+echo; echo "Run 'git-drs --help' for more info"
+
 # Clean up
 rm $TAR_NAME $CHECKSUM_FILE
-
-echo "Installation successful: $DEST/git-drs"
-echo; echo "Run 'git-drs --help' for more info"

b9f57c7cf36c93208a592c558d038ad63307f1c1
Brian Walsh <brian@bwalsh.com>
2026-02-09 14:04:51 -0800

    fix:git-parsing #201

--- lfs/helpers.go
+++ lfs/helpers.go
@@ -70,7 +70,7 @@ func resolveLFSRoot(ctx context.Context, gitCommonDir string) (string, error) {
 
 func runGit(ctx context.Context, args ...string) (string, error) {
 	cmd := exec.CommandContext(ctx, "git", args...)
-	b, err := cmd.CombinedOutput()
+	b, err := cmd.Output()
 	if err != nil {
 		return "", fmt.Errorf("%v: %s", err, strings.TrimSpace(string(b)))
 	}
@@ -87,7 +87,7 @@ func userHomeDir() (string, error) {
 		return home, nil
 	}
 	// macOS/Linux
-	out, err := exec.Command("sh", "-lc", "printf %s \"$HOME\"").CombinedOutput()
+	out, err := exec.Command("sh", "-lc", "printf %s \"$HOME\"").Output()
 	if err != nil {
 		return "", err
 	}
